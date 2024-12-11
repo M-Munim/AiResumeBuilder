a6172f2ed62dd35027daa50a4e03ff2c8ce328ec
munim <muhammadmunimoff330@gmail.com>
2024-12-11 10:16:23 +0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <Button>hello</Button>
+    </>
+  )
+}
+
+export default App
