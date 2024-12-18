d6fdceaab98b860916c554c6475970e3f80a33c1
munim <muhammadmunimoff330@gmail.com>
2024-12-18 20:54:37 +0500

    preview section of resume, also add certificates, hobbies, languages etc

--- src/Dashboard/resume/[resumeId]/edit/index.jsx
+++ src/Dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,15 +1,30 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import FormSection from './components/FormSection';
+import PreviewSection from './components/PreviewSection';
+import { ResumeInfoContext } from '@/context/ResumeInfoContext';
+import { useState } from 'react';
+import dummy from '@/data/dummy';
 
 const EditResume = () => {
   const params = useParams();
+  const [resumeInfo, setResumeInfo] = useState()
 
   useEffect(() => {
-    console.log(params.resumeId);
+    // console.log(params.resumeId);
+    setResumeInfo(dummy)
   }, [])
 
   return (
-    <div>EditResume</div>
+    <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
+      <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
+        {/* Form Section */}
+        <FormSection />
+
+        {/* Preview Section */}
+        <PreviewSection />
+      </div>
+    </ResumeInfoContext.Provider>
   )
 }
 
